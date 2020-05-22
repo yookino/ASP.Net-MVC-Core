9afd57931f78df1e25f5d90b854ccc52034e3716
Itthipon Treetodbadee <yookino@gmail.com>
2020-05-22 23:01:01 +0700

    Item Index

--- WebApplication1/Controllers/ItemController.cs
+++ WebApplication1/Controllers/ItemController.cs
@@ -10,9 +10,29 @@ namespace WebApplication1.Controllers
     public class ItemController : Controller
     {
         // ค้นหาข้อมูล Select * FROM Item
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string itemid, string itemname)
+        {
+            TestContext context = new TestContext();
+            itemname = itemname  == null ? "" : itemname;
+
+            if (itemname == null)
+            {
+                itemname = "";
+            }
+
+            var items = context
+                .Item
+                .Where(i => i.ItemId.ToString().Contains(itemid) 
+                        && i.ItemName.Contains(itemname))
+                .ToList();
+            return View(items);
         }
 
         [HttpGet]
